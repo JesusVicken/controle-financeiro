4cfd1b2328f37b7bb4e13d40e40c9d1bca8d89d7
JesusVicken <jesus.viccken@gmail.com>
2024-07-31 22:13:57 -0300

    feat: UI finlizada

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,21 +2,36 @@ import * as C from "./style";  //carregando todos os styles e atribuindo para C
 import { useState } from "react";
 
 
-const Form = () => {
+const Form = ({ handleAdd }) => {
 
     const [desc, setDesc] = useState('');
     const [amount, setAmount] = useState('');
     const [isExpense, setIsExpense] = useState(false);
 
+    const generateID = () => Math.round(Math.random() * 1000)
+
     const handleSave = () => {
-        if (!desc || amount) {
+        if (!desc || !amount) {
             alert("Informe a descrição e o valor!")
             return;
         } else if (amount < 1) {
             alert("O valor tem q ser positivo!")
             return;
         }
-    }
+
+        const transaction = {
+            id: generateID(),
+            desc: desc,
+            amount: amount,
+            expense: isExpense,
+        };
+
+        handleAdd(transaction);
+
+        setDesc("");
+        setAmount("");
+
+    };
 
     return (
         <>
@@ -39,7 +54,6 @@ const Form = () => {
                         name="group1"
                         onChange={() => setIsExpense(!isExpense)}
                     />
-
                     <C.Label htmlFor="rIncome">Entrada</C.Label>
 
                     <C.Input
@@ -48,12 +62,9 @@ const Form = () => {
                         name="group1"
                         onChange={() => setIsExpense(!isExpense)}
                     />
-
                     <C.Label htmlFor="rIncome">Saída</C.Label>
-
-
                 </C.RadioGroup>
-
+                <C.Button onClick={handleSave}>ADICIONAR</C.Button>
 
 
             </C.Container>
